6470511b837c9a87dae3a8813b0800f3ae67ed26
kesa822 <k.sano0616@gmail.com>
2025-03-03 02:44:28 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,9 @@
+
+//変数の宣言 代入
+let userWight = 68
+let userHeight = 1.7
+let userBmi = userWight / (userHeight * userHeight)
+
+//コンソールへの出力
+console.log(userWight / (userHeight * userHeight));
+console.log(userBmi)
